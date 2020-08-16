616ecf0cb7b282b7c50c2fc5c5e820ab271b2f09
Tomás Hernández <tomas.hernandez03@gmail.com>
2020-08-15 23:25:36 -0400

    Complete test 1

--- src/components/notes-app/index.js
+++ src/components/notes-app/index.js
@@ -1,41 +1,137 @@
-
-
 import React, { Component } from "react";
+import { ALL, ACTIVE, COMPLETED } from "../../constants/noteStatus";
+
 import "./index.css";
 
+let noteId = 0;
+
 export default class NotesApp extends Component {
-  
+  state = {
+    currentFilter: ALL,
+    form: {},
+    notes: {},
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ form: { ...this.state.form, [name]: value } });
+  };
+
+  handleButtonClick = () => {
+    const { form, notes } = this.state;
+    const { title, status } = form;
+    const id = ++noteId;
+
+    this.setState({
+      form: {},
+      notes: { ...notes, [id]: { id, title, status } },
+    });
+  };
+
+  handleFilterClick = (event) => {
+    const { status } = event.target.dataset;
+    this.setState({ currentFilter: status });
+  };
+
   render() {
+    const { currentFilter, notes } = this.state;
+
+    const filteredNotes = Object.values(notes).filter((note) => {
+      if (currentFilter === ALL) return note;
+
+      const status = note.status || "";
+      return status.toLowerCase() === currentFilter;
+    });
+
+    const sortedNotes =
+      currentFilter !== ALL
+        ? filteredNotes
+        : [
+            ...filteredNotes.filter(
+              ({ status = "" }) => status.toLowerCase() === ACTIVE
+            ),
+            ...filteredNotes.filter(
+              ({ status = "" }) => status.toLowerCase() === COMPLETED
+            ),
+            ...filteredNotes.filter(({ status = "" }) => {
+              const currentStatus = status.toLowerCase();
+              return currentStatus !== ACTIVE && currentStatus !== COMPLETED;
+            }),
+          ];
+
     return (
       <div className="layout-column align-items-center justify-content-start">
         <section className="layout-row align-items-center justify-content-center mt-30">
-          <input data-testid="input-note-name" type="text" className="large mx-8"
-                placeholder="Note Title"/>
-          <input data-testid="input-note-status" type="text" className="large mx-8"
-                placeholder="Note Status"/>
-          <button className="" data-testid="submit-button">Add Note</button>
+          <input
+            name="title"
+            data-testid="input-note-name"
+            type="text"
+            value={this.state.form.title || ""}
+            className="large mx-8"
+            placeholder="Note Title"
+            onChange={this.handleInputChange}
+          />
+          <input
+            name="status"
+            data-testid="input-note-status"
+            type="text"
+            value={this.state.form.status || ""}
+            className="large mx-8"
+            placeholder="Note Status"
+            onChange={this.handleInputChange}
+          />
+          <button
+            className=""
+            data-testid="submit-button"
+            onClick={this.handleButtonClick}
+          >
+            Add Note
+          </button>
         </section>
 
         <div className="mt-50">
           <ul className="tabs">
-            <li className="tab-item slide-up-fade-in" data-testid="allButton">All</li>
-            <li className="tab-item slide-up-fade-in" data-testid="activeButton">Active</li>
-            <li className="tab-item slide-up-fade-in" data-testid="completedButton">Completed</li>
+            <li
+              className="tab-item slide-up-fade-in"
+              data-testid="allButton"
+              data-status={ALL}
+              onClick={this.handleFilterClick}
+            >
+              All
+            </li>
+            <li
+              className="tab-item slide-up-fade-in"
+              data-testid="activeButton"
+              data-status={ACTIVE}
+              onClick={this.handleFilterClick}
+            >
+              Active
+            </li>
+            <li
+              className="tab-item slide-up-fade-in"
+              data-testid="completedButton"
+              data-status={COMPLETED}
+              onClick={this.handleFilterClick}
+            >
+              Completed
+            </li>
           </ul>
         </div>
         <div className="card w-40 pt-30 pb-8">
           <table>
             <thead>
-            <tr>
-              <th>Title</th>
-              <th>Status</th>
-            </tr>
+              <tr>
+                <th>Title</th>
+                <th>Status</th>
+              </tr>
             </thead>
             <tbody data-testid="noteList">
-              <tr>
-                <td></td>
-                <td></td>
-              </tr>
+              {sortedNotes.map((note) => (
+                <tr key={note.id}>
+                  <td>{note.title}</td>
+                  <td>{note.status}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
